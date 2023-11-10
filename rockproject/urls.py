f20262d413900673f43fca49370cfe3d8f2b8e07
Sylvia T <sylviarosemarie@gmail.com>
2023-11-10 15:45:15 -0600

    add rock and type views + json data re same - gets acting as expected

--- rockproject/urls.py
+++ rockproject/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from rockapi.views import register_user, login_user
+from rockapi.views import register_user, login_user, TypeView, RockView
 
 router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'types', TypeView, 'type')
+router.register(r'rocks', RockView, 'rock')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('register', register_user),
-    path('login', login_user),
+    path('register', register_user), # Enables http://localhost:8000/register
+    path('login', login_user), # Enables http://localhost:8000/login
     path('admin/', admin.site.urls),
 ]
